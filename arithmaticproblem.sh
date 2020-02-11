a0f7f18809aaa667ba366103b0e06943b2fc7197
paras <parasjitaliya@gmail.com>
2020-02-11 10:52:52 +0530

    Added:user input and print the vlaue

--- arithmaticproblem.sh
+++ arithmaticproblem.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "----------Welcome to Arithmetic Computation----------"
+#Take the user input from user
 read -p "Enter First No:" a
+echo value of a is $a
 read -p "Enter Second No:" b
+echo value of b is $b
 read -p "Enetr Third No:" c
-
+echo value of c is $c
  
